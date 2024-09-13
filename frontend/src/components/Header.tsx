e1ace3b8a6dd1e9b533dfb95f6c51144d575253c
Stefan Novakovic <snovakovic3@gmail.com>
2024-09-13 23:22:50 +0200

    Small tweaks in Header section

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { assets } from '../assets/assets';
 
 const Header = () => {
    return (
-      <div className="flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20">
+      <header className="flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20">
          {/* ----- Left side ----- */}
          <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb[-30px]">
             <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
@@ -25,9 +25,13 @@ const Header = () => {
 
          {/* ----- Right side ----- */}
          <div className="md:w-1/2 relative">
-            <img className="w-full md:absolute bottom-0 h-auto rounded-lg" src={assets.header_img} alt="" />
+            <img
+               className="w-full max-w-[1080px] md:absolute bottom-0 h-auto rounded-lg"
+               src={assets.header_img}
+               alt=""
+            />
          </div>
-      </div>
+      </header>
    );
 };
 export default Header;
