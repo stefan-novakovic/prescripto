046f70a467d2e795ea36f1c57780aa6a0a466db0
Stefan Novakovic <snovakovic3@gmail.com>
2024-09-13 20:27:28 +0200

    SpecialityMenu section added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import Header from '../components/Header';
+import SpecialityMenu from '../components/SpecialityMenu';
 
 const Home = () => {
    return (
       <div>
          <Header />
-         Home Page
+         <SpecialityMenu />
       </div>
    );
 };
