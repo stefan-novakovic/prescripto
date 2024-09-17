285c14e8a6ad2d60112b9edc9cfcf920bd28ea75
Stefan Novakovic <snovakovic3@gmail.com>
2024-09-18 00:13:49 +0200

    Doctors are now sorted by name [A-Z] on Doctors page

--- src/pages/Doctors.tsx
+++ src/pages/Doctors.tsx
@@ -77,23 +77,25 @@ const Doctors = () => {
                </p>
             </div>
             <div className="w-full grid grid-cols-auto gap-4 gap-y-6">
-               {filterDoc.map((doctor, index) => (
-                  <div
-                     key={index}
-                     onClick={() => navigate(`/appointment/${doctor._id}`)}
-                     className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
-                  >
-                     <img className="bg-blue-50" src={doctor.image} alt="" />
-                     <div className="p-4">
-                        <div className="flex items-center gap-2 text-sm text-center text-green-500">
-                           <div className="w-2 h-2 bg-green-500 rounded-full" />
-                           <p>Available</p>
+               {filterDoc
+                  .sort((a, b) => (a.name > b.name ? 1 : -1))
+                  .map((doctor, index) => (
+                     <div
+                        key={index}
+                        onClick={() => navigate(`/appointment/${doctor._id}`)}
+                        className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+                     >
+                        <img className="bg-blue-50" src={doctor.image} alt="" />
+                        <div className="p-4">
+                           <div className="flex items-center gap-2 text-sm text-center text-green-500">
+                              <div className="w-2 h-2 bg-green-500 rounded-full" />
+                              <p>Available</p>
+                           </div>
+                           <p className="text-gray-900 text-lg font-medium">{doctor.name}</p>
+                           <p className="text-gray-600 text-sm">{doctor.speciality}</p>
                         </div>
-                        <p className="text-gray-900 text-lg font-medium">{doctor.name}</p>
-                        <p className="text-gray-600 text-sm">{doctor.speciality}</p>
                      </div>
-                  </div>
-               ))}
+                  ))}
             </div>
          </div>
       </div>
