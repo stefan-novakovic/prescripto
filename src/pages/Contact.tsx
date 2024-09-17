7efd07a5a72b6468288a547babb0b62e8ff13bf1
Stefan Novakovic <snovakovic3@gmail.com>
2024-09-18 00:28:19 +0200

    Contact page created

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,34 @@
+import { assets } from '../assets/assets';
+
 const Contact = () => {
-   return <div className="flex flex-1 flex-col">Contact Page</div>;
+   return (
+      <div className="flex flex-1 flex-col">
+         <div className="text-center text-2xl pt-10 text-gray-500">
+            <p>
+               CONTACT <span className="text-gray-700 font-semibold">US</span>
+            </p>
+         </div>
+
+         <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
+            {/* ----- Left Side ----- */}
+            <img className="w-full md:max-w-[360px]" src={assets.contact_image} alt="" />
+            {/* ----- Right Side ----- */}
+            <div className="flex flex-col justify-center items-start gap-6">
+               <p className="font-semibold text-lg text-gray-600">OUR OFFICE</p>
+               <p className="text-gray-500">
+                  54709 Willms Station <br /> Suite 350, Washington, USA
+               </p>
+               <p className="text-gray-500">
+                  Tel: (415) 555-0132 <br /> Email: greatstackdev@gmail.com
+               </p>
+               <p className="font-semibold text-lg text-gray-600">Careers at PRESCRIPTO</p>
+               <p className="text-gray-500">Learn more about our teams and job openings.</p>
+               <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+                  Explore Jobs
+               </button>
+            </div>
+         </div>
+      </div>
+   );
 };
 export default Contact;
