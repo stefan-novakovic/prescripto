e38f11fedfa00c3468763d3b1b6cf52f78c5ce29
Stefan Novakovic <snovakovic3@gmail.com>
2024-09-21 00:25:47 +0200

    Responsive changes (header and banner sections)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,8 +7,8 @@ const Banner = () => {
    return (
       <section className="flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10">
          {/* ----- Left side ----- */}
-         <div className="flex-1 py-8 sm:py-10 md:py-16 lg:py-24 lg:pl-5">
-            <div className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white">
+         <div className="flex flex-col items-center md:items-start flex-1 py-8 sm:py-10 md:py-14 lg:py-18 xl:py-24 lg:pl-5">
+            <div className="text-center md:text-start text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white">
                <p>Book Appointment</p>
                <p className="mt-4">With 100+ Trusted Doctors</p>
             </div>
@@ -24,7 +24,7 @@ const Banner = () => {
          </div>
 
          {/* ----- Right side ----- */}
-         <div className="hidden md:block md:w-1/2 lg:w-[370px] relative">
+         <div className="hidden ml-8 md:block md:w-1/2 lg:w-[370px] relative">
             <img className="w-full absolute border-0 bottom-0 right-0 max-w-md" src={assets.appointment_img} alt="" />
          </div>
       </section>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,11 @@ const Header = () => {
    return (
       <header className="flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20">
          {/* ----- Left side ----- */}
-         <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb[-30px]">
-            <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
+         <div className="md:w-1/2 flex flex-col items-center md:items-start justify-center gap-4 py-10 m-auto md:py-[5vw] lg:py-[7vw] xl:py-[10vw] md:mb[-30px]">
+            <p className="text-center md:text-start text-3xl lg:text-4xl xl:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
                Book Appointment <br /> With Trusted Doctors
             </p>
-            <div className="flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light">
+            <div className="flex flex-col md:flex-row items-center gap-3 text-center md:text-start text-white text-sm font-light">
                <img className="w-28" src={assets.group_profiles} alt="" />
                <p>
                   Simply browse through our extensive list of trusted doctors, <br className="hidden sm:block" />
