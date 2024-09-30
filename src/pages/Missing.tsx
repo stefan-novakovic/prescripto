29e9276e0da718f2e7586fd585c7e8e8edd60d64
Stefan Novakovic <snovakovic3@gmail.com>
2024-09-30 21:36:45 +0200

    Missing page created

--- src/pages/Missing.tsx
+++ src/pages/Missing.tsx
@@ -1,4 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Missing = () => {
-   return <div className="flex flex-1 flex-col">Missing Page</div>;
+   return (
+      <div className="flex flex-1 flex-col">
+         <h1 className="text-3xl font-bold my-6">Page Not Found</h1>
+         <p className="mb-2">Sorry, the page you are looking for does not exist.</p>
+         <Link to="/" className="w-fit text-primary hover:underline">
+            Go to Home
+         </Link>
+      </div>
+   );
 };
 export default Missing;
