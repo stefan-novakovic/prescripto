28ee2d0226c817a32c5c499e24e971be773449aa
Stefan Novakovic <snovakovic3@gmail.com>
2024-09-30 19:56:13 +0200

    Responsive fix

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,7 +11,9 @@ const About = () => {
 
          <div className="my-10 flex flex-col md:flex-row gap-12">
             {/* ----- Left Side ----- */}
-            <img className="w-full md:max-w-[360px]" src={assets.about_image} alt="" />
+            <div>
+               <img className="w-full md:max-w-[360px]" src={assets.about_image} alt="" />
+            </div>
             {/* ----- Right Side ----- */}
             <div className="flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600">
                <p>
@@ -41,15 +43,15 @@ const About = () => {
          </div>
 
          <div className="flex flex-col md:flex-row mb-20">
-            <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+            <div className="border px-10 lg:px-16 py-8 lg:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
                <b>Efficiency:</b>
                <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
             </div>
-            <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+            <div className="border px-10 lg:px-16 py-8 lg:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
                <b>Convenience:</b>
                <p>Access to a network of trusted healthcare professionals in your area.</p>
             </div>
-            <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+            <div className="border px-10 lg:px-16 py-8 lg:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
                <b>Personalization:</b>
                <p>Tailored recommendations and reminders to help you stay on top of your health.</p>
             </div>
