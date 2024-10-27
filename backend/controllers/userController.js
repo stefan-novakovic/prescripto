cf5d55394a8b91da3d66f3a8a84259a6872dea00
Stefan Novakovic <snovakovic3@gmail.com>
2024-10-27 20:25:16 +0100

    Created user login API

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,4 +44,28 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+// API from user login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser, loginUser };
