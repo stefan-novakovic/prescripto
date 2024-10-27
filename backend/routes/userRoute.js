578247a6596c60c3dd4641bb6c6bab4c06cf9884
Stefan Novakovic <snovakovic3@gmail.com>
2024-10-28 00:06:46 +0100

    Created get user profile and update user profile APIs

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,24 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/userController.js";
+import {
+  registerUser,
+  loginUser,
+  getProfile,
+  updateProfile,
+} from "../controllers/userController.js";
+import authUser from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+
+userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post(
+  "/update-profile",
+  upload.single("image"),
+  authUser,
+  updateProfile
+);
 
 export default userRouter;
