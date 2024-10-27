2f959c1c340d90f370396284cb4c64acf6512879
Stefan Novakovic <snovakovic3@gmail.com>
2024-10-27 20:10:48 +0100

    Created user registration API

---
+++ backend/controllers/userController.js
@@ -0,0 +1,47 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+// API to register user
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email & !password) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+
+    // validating email format
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Enter a valid email" });
+    }
+
+    // validating strong password
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Enter a strong password" });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser };
