c807246af6d7a30544a8639f0b76720009cda47c
Stefan Novakovic <snovakovic3@gmail.com>
2024-12-24 21:32:32 +0100

    About page css fix

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -2,20 +2,20 @@ import { assets } from '../assets/assets';
 
 const About = () => {
    return (
-      <section className="flex flex-1 flex-col">
-         <div className="text-center text-2xl pt-10 text-gray-500">
+      <section className="flex flex-1 flex-col w-full 2xl:max-w-[1650px] 2xl:mx-auto">
+         <div className="text-start text-2xl pt-4 md:pt-7 text-gray-500">
             <p>
                ABOUT <span className="text-gray-700 font-medium">US</span>
             </p>
          </div>
 
-         <div className="my-10 flex flex-col justify-center md:flex-row gap-12">
+         <div className="my-10 flex flex-col justify-center md:flex-row gap-8 md:gap-12">
             {/* ----- Left Side ----- */}
-            <div>
-               <img className="w-full md:max-w-[360px]" src={assets.about_image} alt="" />
+            <div className="w-full md:w-[378px] aspect-[378/384.33] h-min">
+               <img className="w-full md:w-[378px]" src={assets.about_image} alt="" />
             </div>
             {/* ----- Right Side ----- */}
-            <div className="flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600">
+            <div className="flex flex-col justify-center gap-6 md:w-2/5 text-sm text-gray-600">
                <p>
                   Welcome to Prescripto, your trusted partner in managing your healthcare needs conveniently and
                   efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling
@@ -42,7 +42,7 @@ const About = () => {
             </p>
          </div>
 
-         <div className="flex flex-col md:flex-row mb-20">
+         <div className="flex flex-col md:flex-row mb:8 md:mb-12">
             <div className="border px-10 lg:px-16 py-8 lg:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
                <b>Efficiency:</b>
                <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
