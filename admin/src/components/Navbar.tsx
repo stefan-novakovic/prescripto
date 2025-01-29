85330d85c4cb03b1de4558eed9c0dc882e2f3c57
Stefan Novakovic <snovakovic3@gmail.com>
2025-01-30 00:41:10 +0100

    Navbar component css fix; created custom breakpoint in taiwind.config.js (375px)

--- admin/src/components/Navbar.tsx
+++ admin/src/components/Navbar.tsx
@@ -22,12 +22,20 @@ const Navbar = () => {
    return (
       <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white">
          <div className="flex items-center gap-2 text-xs">
-            <img onClick={() => navigate('/')} className="w-36 sm:w-40 cursor-pointer" src={assets.admin_logo} alt="" />
-            <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">
+            <div className="w-36 h-[34.078px] sm:w-[148px] sm:h-[37.859px]">
+               <img
+                  onClick={() => navigate('/')}
+                  className="w-36 sm:w-40 cursor-pointer"
+                  src={assets.admin_logo}
+                  alt=""
+               />
+            </div>
+
+            <p className="hidden c375:block border px-2.5 py-0.5 mr-2 rounded-full border-gray-500 text-gray-600">
                {aToken ? 'Admin' : 'Doctor'}
             </p>
          </div>
-         <button onClick={logout} className="bg-primary text-white text-sm px-10 py-2 rounded-full">
+         <button onClick={logout} className="bg-primary text-white text-sm px-9 sm:px-10 py-2 rounded-full">
             Logout
          </button>
       </div>
