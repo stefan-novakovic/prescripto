59f786a9a0e4766022dda530cebdadd26207f019
Stefan Novakovic <snovakovic3@gmail.com>
2025-02-01 13:50:15 +0100

    Sidebar css fix

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -22,7 +22,7 @@ const App = () => {
       <div className="bg-[#F8F9FD]">
          <ToastContainer />
          <Navbar />
-         <div className="flex items-start">
+         <div className="flex flex-col sm:flex-row items-start">
             <Sidebar />
 
             <Routes>
