a3d768198897636e89f4d0290b99bfaaed805573
Stefan Novakovic <snovakovic3@gmail.com>
2025-02-03 22:58:52 +0100

    Admin Dashboard page css fixes (layout shift prevention)

--- admin/src/pages/Admin/Dashboard.tsx
+++ admin/src/pages/Admin/Dashboard.tsx
@@ -18,25 +18,31 @@ const Dashboard = () => {
          <section className="h-[calc(100vh-61px-48px)] sm:h-[calc(100vh-62.85px)] overflow-y-scroll px-2.5 sm:px-5 py-4 w-full">
             <div className="flex flex-wrap gap-3">
                <div className="w-full c375:w-auto flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 hover:scale-105 transition-all">
-                  <img className="w-14" src={assets.doctor_icon} alt="" />
+                  <div className="w-14 aspect-square">
+                     <img className="w-14 h-14" src={assets.doctor_icon} alt="" />
+                  </div>
                   <div>
-                     <p className="text-xl font-semibold text-gray-600">{dashData.doctors}</p>
+                     <p className="text-xl font-semibold min-h-7 text-gray-600">{dashData.doctors}</p>
                      <p className="text-gray-400">Doctors</p>
                   </div>
                </div>
 
                <div className="w-full c375:w-auto flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 hover:scale-105 transition-all">
-                  <img className="w-14" src={assets.appointments_icon} alt="" />
+                  <div className="w-14 aspect-square">
+                     <img className="w-14 h-14" src={assets.appointments_icon} alt="" />
+                  </div>
                   <div>
-                     <p className="text-xl font-semibold text-gray-600">{dashData.appointments}</p>
+                     <p className="text-xl font-semibold min-h-7 text-gray-600">{dashData.appointments}</p>
                      <p className="text-gray-400">Appointments</p>
                   </div>
                </div>
 
                <div className="w-full c375:w-auto flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 hover:scale-105 transition-all">
-                  <img className="w-14" src={assets.patients_icon} alt="" />
+                  <div className="w-14 aspect-square">
+                     <img className="w-14 h-14" src={assets.patients_icon} alt="" />
+                  </div>
                   <div>
-                     <p className="text-xl font-semibold text-gray-600">{dashData.patients}</p>
+                     <p className="text-xl font-semibold min-h-7 text-gray-600">{dashData.patients}</p>
                      <p className="text-gray-400">Patients</p>
                   </div>
                </div>
@@ -44,17 +50,21 @@ const Dashboard = () => {
 
             <div className="bg-white">
                <div className="flex items-center gap-2.5 px-4 py-4 mt-4 sm:mt-8 rounded-t border">
-                  <img src={assets.list_icon} alt="" />
+                  <div className="w-[22px] aspect-square">
+                     <img src={assets.list_icon} className="w-[22px] h-[22px]" alt="" />
+                  </div>
                   <p className="font-semibold">Latest Bookings</p>
                </div>
 
                <div className="pt-4 border border-t-0">
                   {dashData.latestAppointments.map((item, index) => (
                      <div className="flex items-center px-6 py-3 gap-3 hover:bg-gray-100" key={index}>
-                        <img className="rounded-full w-10" src={item.docData.image} alt="" />
+                        <div className="w-10 aspect-square">
+                           <img className="rounded-full w-10 h-10" src={item.docData.image} alt="" />
+                        </div>
                         <div className="flex-1 text-sm">
-                           <p className="text-gray-800 font-medium">{item.docData.name}</p>
-                           <p className="text-gray-600">{slotDateFormat(item.slotDate)}</p>
+                           <p className="text-gray-800 min-h-5 font-medium">{item.docData.name}</p>
+                           <p className="text-gray-600 min-h-5">{slotDateFormat(item.slotDate)}</p>
                         </div>
 
                         {item.cancelled ? (
@@ -62,12 +72,14 @@ const Dashboard = () => {
                         ) : item.isCompleted ? (
                            <p className="text-green-500 min-h-10 flex items-center text-xs font-medium">Completed</p>
                         ) : (
-                           <img
-                              onClick={() => cancelAppointment(item._id)}
-                              className="w-10 cursor-pointer"
-                              src={assets.cancel_icon}
-                              alt=""
-                           />
+                           <div className="w-10 aspect-square">
+                              <img
+                                 onClick={() => cancelAppointment(item._id)}
+                                 className="w-10 h-10 cursor-pointer"
+                                 src={assets.cancel_icon}
+                                 alt=""
+                              />
+                           </div>
                         )}
                      </div>
                   ))}
